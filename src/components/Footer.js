8a58ab4518d67f79c083778524bccf26f0ae1134
snjstudent <umi.sea.0315@icloud.com>
2019-08-25 15:19:44 +0900

    create UI

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,6 +5,7 @@ import ExpansionPanelSummary from "@material-ui/core/ExpansionPanelSummary";
 import ExpansionPanelDetails from "@material-ui/core/ExpansionPanelDetails";
 import Typography from "@material-ui/core/Typography";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
+import { flexbox } from "@material-ui/system";
 
 const useStyles = makeStyles(theme => ({
   root: {
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,12 +1,106 @@
 import React from "react";
+import clsx from "clsx";
+import { makeStyles } from "@material-ui/core/styles";
+import MenuItem from "@material-ui/core/MenuItem";
+import TextField from "@material-ui/core/TextField";
+import Button from "@material-ui/core/Button";
+import { Link } from "react-router-dom";
+import InputLabel from "@material-ui/core/InputLabel";
+import InputAdornment from "@material-ui/core/InputAdornment";
+import FormHelperText from "@material-ui/core/FormHelperText";
+import FormControl from "@material-ui/core/FormControl";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+import IconButton from "@material-ui/core/IconButton";
+import Input from "@material-ui/core/Input";
 
-import { Link } from 'react-router-dom'
+const useStyles = makeStyles(theme => ({
+  container: {
+    flexWrap: "wrap",
+    paddingTop: "0.3rem",
+    paddingBottom: "3rem",
+    textAlign: "center"
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 300,
+    marginTop: "4rem"
+  },
+  dense: {
+    marginTop: 19
+  },
+  menu: {
+    width: 200
+  },
+  button: {
+    margin: theme.spacing(1),
+    marginBottom: "3rem"
+  },
+  input: {
+    display: "none"
+  }
+}));
 
-const Login = () => (
-    <div>
+const Login = () => {
+  const classes = useStyles();
+  const [values, setValues] = React.useState({
+    amount: "",
+    password: "",
+    weight: "",
+    weightRange: "",
+    showPassword: false
+  });
+  const handleClickShowPassword = () => {
+    setValues({ ...values, showPassword: !values.showPassword });
+  };
+
+  const handleMouseDownPassword = event => {
+    event.preventDefault();
+  };
+  const handleChange = name => event => {
+    setValues({ ...values, [name]: event.target.value });
+  };
+
+  return (
+    <div className="loginbox">
+      <p>Login</p>
+      <form className={classes.container} noValidate autoComplete="off">
+        <TextField
+          id="standard-name"
+          label="User Name"
+          className={classes.textField}
+          margin="normal"
+        />
+        <br />
+        <FormControl className={clsx(classes.margin, classes.textField)}>
+          <InputLabel htmlFor="adornment-password">Password</InputLabel>
+          <Input
+            id="adornment-password"
+            type={values.showPassword ? "text" : "password"}
+            value={values.password}
+            onChange={handleChange("password")}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                >
+                  {values.showPassword ? <Visibility /> : <VisibilityOff />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+      </form>
+      <Button variant="outlined" color="secondary" className={classes.button}>
         Login
-        <p><Link to="/">メイン</Link></p>
+      </Button>
+      <br />
+      <Link to="/">メイン</Link>
     </div>
-)
+  );
+};
 
 export default Login;
